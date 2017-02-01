76dc13736a0b9d363a9b0e5d0fc0180942701a24
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2017-02-01 12:22:59 +0000

    hack: Fix the header replacement in the previous hack
    Authors: @richardtowers

--- src/main/java/uk/gov/ida/eidas/bridge/hacks/BrokenContentTypeWorkaroundJerseyFilter.java
+++ src/main/java/uk/gov/ida/eidas/bridge/hacks/BrokenContentTypeWorkaroundJerseyFilter.java
@@ -5,6 +5,7 @@ import org.apache.http.HttpHeaders;
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientResponseContext;
 import javax.ws.rs.client.ClientResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,17 +13,19 @@ import java.util.List;
 public class BrokenContentTypeWorkaroundJerseyFilter implements ClientResponseFilter {
     @Override
     public void filter(ClientRequestContext requestContext, ClientResponseContext responseContext) throws IOException {
-        List<Object> fixedContentTypeHeaders = new ArrayList<>();
-        List<String> contentTypeHeaders = responseContext.getHeaders().get(HttpHeaders.CONTENT_TYPE);
+        List<String> fixedContentTypeHeaders = new ArrayList<>();
+        MultivaluedMap<String, String> headers = responseContext.getHeaders();
+        List<String> contentTypeHeaders = headers.get(HttpHeaders.CONTENT_TYPE);
 
         for (String header : contentTypeHeaders) {
             if (header.startsWith("application;")) {
                 // This header is broken. It should have a forward slash (/) before the semicolon
                 String fixedHeader = header.replaceFirst("application;", "application/xml;");
-                fixedContentTypeHeaders.add(fixedHeader); } else {
+                fixedContentTypeHeaders.add(fixedHeader);
+            } else {
                 fixedContentTypeHeaders.add(header);
             }
         }
-        requestContext.getHeaders().replace(HttpHeaders.CONTENT_TYPE, fixedContentTypeHeaders);
+        headers.replace(HttpHeaders.CONTENT_TYPE, fixedContentTypeHeaders);
     }
 }
